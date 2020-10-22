653d1c3729150f475271409613f1412620bf76eb
Dimitri <dma2012@gmail.com>
2020-10-22 08:04:18 -0400

    added primary key id

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,11 @@
 USE codeup_test_db;
 DROP TABLE albums;
 CREATE TABLE albums (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(50),
-  name VARCHAR(50),
-  release_date date,
-  sales float,
-  genre VARCHAR(50),
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date date,
+    sales float,
+    genre VARCHAR(50),
+    PRIMARY KEY (id),
 );
