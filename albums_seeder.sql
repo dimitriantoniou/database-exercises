5863d6de288ccd6ee7b7a156343484dcccc9295c
Dimitri <dma2012@gmail.com>
2020-10-22 08:16:44 -0400

    insert seeder data

---
+++ albums_seeder.sql
@@ -0,0 +1,5 @@
+USE codeup_test_db;
+--INSERT INTO table_name (field1, field2, ...)
+--VALUES ('value1', 'value2', ...);
+INSERT INTO albums (artist, name, sales);
+VALUES ('ACDC','Back In Black',47300000);
