7924c32cfaa7a5ffedce683380f4920aa5e8327c
Dimitri <dma2012@gmail.com>
2020-10-22 08:03:33 -0400

    created table albums with columns

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,10 @@
 USE codeup_test_db;
+DROP TABLE albums;
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(50),
+  name VARCHAR(50),
+  release_date date,
+  sales float,
+  genre VARCHAR(50),
+);
