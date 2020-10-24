b6a7ccb5101ce429994e700525200cfb8cb71b7f
Dimitri <dma2012@gmail.com>
2020-10-24 09:06:29 -0400

    where clauses with multiple criteria

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,12 +6,22 @@ Create a file named where_exercises.sql. Make sure to use the employees database
 --Find all employees born on Christmas — 842 rows.
 --Find all employees with a 'q' in their last name — 1,873 rows.
 
+Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+Find all employees hired in the 90s and born on Christmas — 362 rows.
+Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
  */
 USE employees;
 SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE first_name='Irena'OR first_name = 'Vidya' OR first_name='Maya';
+SELECT * FROM employees WHERE (first_name='Irena'OR first_name = 'Vidya' OR first_name='Maya') AND (gender='M');
 
 USE employees;
 SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND '%E';
 
 USE employees;
 SELECT * FROM employees WHERE hire_date BETWEEN 1990-01-01 AND 1990-12-31;
@@ -20,4 +30,8 @@ USE employees;
 SELECT * FROM employees WHERE birth_date LIKE'%12-25';
 
 USE employees;
+SELECT * FROM employees WHERE (hire_date BETWEEN 1990-01-01 AND 1990-12-31) AND birth_date LIKE '%12-25';;
+
+USE employees;
 SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE (last_name LIKE '%q%') AND (last_name NOT LIKE '%qu%');
